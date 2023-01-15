f49000f559d8d9ded5f2232cb524ff48256a6edb
Zakaria Tobbal <tzakaria@hotmail.fr>
2023-01-15 20:45:45 +0100

    Importing the right dependencies and moving Main class to the project package

---
+++ src/main/java/amraoui_tobbal/project/Main.java
@@ -0,0 +1,15 @@
+package amraoui_tobbal.project;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+ 
